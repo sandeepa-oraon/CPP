699bc6d27301a7f0349f43f58f587e08be53c1d9
Sandy2004 <134693152+sandeepa-oraon@users.noreply.github.com>
2025-05-28 22:12:09 +0530

    Update nonRepeated.cpp

--- LeetCode/PG/NonRepeated/nonRepeated.cpp
+++ LeetCode/PG/NonRepeated/nonRepeated.cpp
@@ -1,1 +1,17 @@
-
+#include<iostream>
+int main() {
+    int size;
+    int ar1[]={2, 5, 3, 3, 2};
+    size= sizeof(ar1)/sizeof(ar1[0]);
+    for(int i=0; i<size; i++){
+        int isUnique= true;
+        for(int j=0; j<size; j++ ){
+            if(i != j && ar1[i] == ar1[j]){
+                isUnique= false;
+                break;
+            }
+        }
+        if(isUnique)
+            cout<<ar1[i]<<" ";
+    }
+}
