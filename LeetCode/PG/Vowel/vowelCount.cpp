f98aca55aa1afb4a51088cd81b1b6b47a98ae838
Sandy2004 <134693152+sandeepa-oraon@users.noreply.github.com>
2025-05-28 22:02:06 +0530

    Update vowelCount.cpp

--- LeetCode/PG/Vowel/vowelCount.cpp
+++ LeetCode/PG/Vowel/vowelCount.cpp
@@ -1,1 +1,19 @@
-
+int main() {
+    // string chr= "Aello World";
+    string chr;
+    int count=0;
+    cout<<"Enter string: ";
+    getline(cin, chr);
+    cout<<"Consonents are: \n";
+    for(int i=0; i<chr.length(); i++){
+        char ch= tolower(chr[i]);
+        if(ch== 'a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
+            count++;
+        }
+        else{
+            cout<<chr[i]<<" ";
+        }
+    }
+    cout<<"\nNumber of Vowels: "<<count;
+    
+}
