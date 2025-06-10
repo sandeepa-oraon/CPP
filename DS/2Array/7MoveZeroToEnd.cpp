1f35cb66179e77103756b92b4487d7b36c986489
Sandeepa Oraon <sandeepa2004oraon@gmail.com>
2025-06-10 23:49:34 +0530

    v1

--- DS/2Array/7MoveZeroToEnd.cpp
+++ DS/2Array/7MoveZeroToEnd.cpp
@@ -1,3 +1,15 @@
+/*
+Example 1:
+Input: 1 ,0 ,2 ,3 ,0 ,4 ,0 ,1
+Output: 1 ,2 ,3 ,4 ,1 ,0 ,0 ,0
+Explanation: All the zeros are moved to the end and non-negative integers are moved to front by maintaining order
+
+Example 2:
+Input: 1,2,0,1,0,4,0
+Output: 1,2,1,4,0,0,0
+Explanation: All the zeros are moved to the end and non-negative integers are moved to front by maintaining order
+*/
+
 #include<iostream>
 #include<vector>
 using namespace std;
